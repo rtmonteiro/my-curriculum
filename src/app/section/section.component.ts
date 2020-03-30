954fd4f7a453a4e565b8df7e50e91dce40e50724
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-03-30 19:05:33 -0300

    starting section component

---
+++ src/app/section/section.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { faCrosshairs } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent implements OnInit {
+
+  faCoffe = faCrosshairs;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
