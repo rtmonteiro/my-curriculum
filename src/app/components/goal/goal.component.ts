f42aab05c02ec1472b4c13e465c5335bad803fff
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-04-07 19:06:32 -0300

    Fixed the layout problem of Goal Component

--- src/app/components/goal/goal.component.ts
+++ src/app/components/goal/goal.component.ts
@@ -11,6 +11,8 @@ export class GoalComponent implements OnInit {
   head_title = 'Objetivos Profissionais';
   head_icon = faCrosshairs;
 
+  goal_text = 'Almejo a vaga de estágio de Desenvolvedor e através deste cargo, me desafiar em novas atividades que envolvam o aprendizado de novas frameworks, que utilizem a computação na nuvem, onde haja a aplicação de métodos ágeis de gerenciamento de equipes e eficiência (Scrum, GTD), e que exija a busca por inovação e agilidade para a solução de problemas. Eu realmente procuro iniciar a minha vida profissional dentro de uma empresa que me desafie e que eu posso de alguma forma contribuir para o avanço dela.';
+
   constructor(private el:ElementRef){}
 
   show(){
@@ -20,7 +22,11 @@ export class GoalComponent implements OnInit {
     console.log(`width---${this.el.nativeElement.offsetWidth}`);
   }
 
+  ngDoCheck(): void {
+  }
+
   ngOnInit(): void {
+
   }
 
 }
